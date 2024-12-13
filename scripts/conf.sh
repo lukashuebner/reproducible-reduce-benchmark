a90997e7fe4986c6f2f8ec527cbad1bd37b8c838
Lukas Hübner <lukas@klhuebner.de>
2024-12-13 13:08:56 +0100

    Switch benchmark.sbatch to single executable repro-raxml-ng binary

--- scripts/conf.sh
+++ scripts/conf.sh
@@ -7,7 +7,4 @@ export DATA_DIR="data"
 export OUTPUT_DIR="output"
 
 export REFERENCE_RAXML_NG="./reference-raxml-ng"
-export R2D2_RAXML_NG="./r2d2-raxml-ng"
-export REPROBLAS_RAXML_NG="./reproblas-raxml-ng"
-export REPROGATHER_RAXML_NG="./reprogather-raxml-ng"
-export REDUCEBCAST_RAXML_NG="./reducebcast-raxml-ng"
+export REPRODUCIBLE_RAXML_NG="./reproducible-raxml-ng"
